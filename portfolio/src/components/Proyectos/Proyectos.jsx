47c655074940f8b8bd31d5f5d4cb9346b68660d9
gernavarrete <germannavarrete.info@gmail.com>
2023-10-30 18:43:34 -0300

    update portfolio

--- portfolio/src/components/Proyectos/Proyectos.jsx
+++ portfolio/src/components/Proyectos/Proyectos.jsx
@@ -3,7 +3,8 @@ import s from "../Proyectos/Proyectos.module.css";
 import Slider from "../Slider/Slider";
 import mangiare from "../../images/Mangiare-min.png";
 import foodapi from "../../images/FoodApi.png";
-import starwars from "../../images/screenshot1.png"
+import starwars from "../../images/screenshot1.png";
+import fullstore from "../../images/fullstore.png";
 
 export default function Proyectos() {
   const Proyects = [
@@ -12,7 +13,7 @@ export default function Proyectos() {
       img: mangiare,
       linkapp: "https://mangiare.vercel.app/",
       description:
-      "Este proyecto nació con el objetivo de brindar una solución a la pregunta que muchas familias se hacen diariamente: ¿Qué cocinar hoy? Sabemos que el tiempo para planear la comida es escaso, especialmente cuando ambos padres trabajan o tienen otras actividades. Por eso, decidimos crear una plataforma que les permita encontrar una receta con los ingredientes que poseen y, en caso de que falte alguno, adquirirlo de manera fácil y rápida. En resumen, nuestro proyecto busca simplificar la vida de las personas al momento de cocinar, ofreciéndoles una solución práctica y eficiente para que puedan disfrutar de una comida deliciosa sin tener que preocuparse por la planificación.",
+        "Este proyecto nació con el objetivo de brindar una solución a la pregunta que muchas familias se hacen diariamente: ¿Qué cocinar hoy? Sabemos que el tiempo para planear la comida es escaso, especialmente cuando ambos padres trabajan o tienen otras actividades. Por eso, decidimos crear una plataforma que les permita encontrar una receta con los ingredientes que poseen y, en caso de que falte alguno, adquirirlo de manera fácil y rápida. En resumen, nuestro proyecto busca simplificar la vida de las personas al momento de cocinar, ofreciéndoles una solución práctica y eficiente para que puedan disfrutar de una comida deliciosa sin tener que preocuparse por la planificación.",
       tecnologias:
         "Front: Javascript, React, Redux, Auth0, Nodemailer, Cloudinary, Localstorage, ChakraUi. Back: Javascript, Node.js, Express.js Base de datos: PostgreSQL y Sequelize.",
     },
@@ -33,6 +34,14 @@ export default function Proyectos() {
         "Star Wars Explorer es una Single Page Application (SPA) que te permite explorar el emocionante mundo de la saga Star Wars. Con esta aplicación, podrás consultar toda la información relacionada con tus personajes, naves, planetas y más, a través de una búsqueda por nombre fácil y rápida.",
       tecnologias:
         "Front: HTML, CSS, Javascript, React, Next.js , Redux-toolkit, MaterialUI,",
+    },
+    {
+      title: "FullStore",
+      img: fullstore,
+      linkapp: "https://gernavarrete.github.io/fullstore.github.io",
+      description:
+        "Es una tienda online de aparatos de electrónica con el objetivo de poder encontrar precios actualizados de los últimos dispositivos del mercado y coordinar su compra con un vendedor personalizado",
+      tecnologias: "Front: HTML, CSS, Javascript",
     },
   ];
   return (
